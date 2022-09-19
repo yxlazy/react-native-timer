629ea63a17177d195d0971d5d35e4729bdaf43af
Li <1977735808@qq.com>
2022-09-20 00:21:29 +0800

    feat: add data function

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,15 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React, {Fragment, useEffect, useState} from 'react';
 import {
   FlatList,
+  Modal,
+  Pressable,
   StyleSheet,
   Text,
+  TextInput,
   useWindowDimensions,
   View,
 } from 'react-native';
 import Button from '../../components/button';
 import Empty from '../../components/empty';
 import Typography from '../../components/typography';
-import {Color} from '../../constants/styles';
+import {Color, FontColor} from '../../constants/styles';
 import formatHHMMSS from '../../utils/formatHHMMSS';
 import timeMeter from '../../utils/timeMeter';
 
@@ -20,7 +23,10 @@ const Home = () => {
   const [showPause, setShowPause] = useState(false);
   const [hasRest, setHasRest] = useState(false);
   const {height} = useWindowDimensions();
-  const [dataSource] = useState<
+  const [visible, setVisible] = useState(false);
+  const [text, setText] = useState('');
+
+  const [dataSource, setDataSource] = useState<
     Array<{
       id: number;
       content: string;
@@ -43,6 +49,28 @@ const Home = () => {
     timer && timer.init(setCount);
     setHasRest(false);
     setShowPause(false);
+    onPressModalOpen();
+  };
+
+  // Modal
+  const onPressModalOpen = () => {
+    setVisible(true);
+  };
+
+  const onPressModalClose = () => {
+    setVisible(false);
+    setDataSource(data => {
+      data.push({
+        id: Date.now(),
+        content: text,
+      });
+
+      return [...data];
+    });
+  };
+
+  const onChangeText = (value: string) => {
+    setText(value);
   };
 
   useEffect(() => {
@@ -52,41 +80,58 @@ const Home = () => {
   }, []);
 
   return (
-    <View style={{...styles.container, minHeight: height}}>
-      {/* 内容 */}
-      <FlatList
-        ListEmptyComponent={<Empty />}
-        data={dataSource}
-        renderItem={({item}) => (
-          <View style={styles.renderItem}>
-            <Typography>{item.content}</Typography>
+    <Fragment>
+      <View style={{...styles.container, minHeight: height}}>
+        {/* 内容 */}
+        <FlatList
+          ListEmptyComponent={<Empty />}
+          data={dataSource}
+          renderItem={({item}) => (
+            <View style={styles.renderItem}>
+              <Typography>{item.content}</Typography>
+            </View>
+          )}
+          // refreshing
+          style={styles.list}
+        />
+        {/* 计时器 */}
+        {hasRest && (
+          <View style={styles.timerContentWrapper}>
+            <Text style={styles.timerContent}>{formatHHMMSS(count)}</Text>
           </View>
         )}
-        // refreshing
-        style={styles.list}
-      />
-      {/* 计时器 */}
-      {hasRest && (
-        <View style={styles.timerContentWrapper}>
-          <Text style={styles.timerContent}>{formatHHMMSS(count)}</Text>
+        {/* 按钮 */}
+        <View style={{...styles.btnsWrapper, top: height - 100}}>
+          {showPause ? (
+            <Button text="暂停" onPress={onPressPause} />
+          ) : (
+            <Button text={hasRest ? '继续' : '开始'} onPress={onPressStart} />
+          )}
+          <Button text="结束" onPress={onPressEnd} disabled={!hasRest} />
         </View>
-      )}
-      {/* 按钮 */}
-      <View style={{...styles.btnsWrapper, top: height - 100}}>
-        {showPause ? (
-          <Button text="暂停" onPress={onPressPause} />
-        ) : (
-          <Button text={hasRest ? '继续' : '开始'} onPress={onPressStart} />
-        )}
-        <Button text="结束" onPress={onPressEnd} disabled={!hasRest} />
       </View>
-    </View>
+      <Modal visible={visible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <Text style={styles.title}>输入名称</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            autoFocus
+          />
+        </View>
+        <Pressable onPress={onPressModalClose} style={styles.modalBtnWrapper}>
+          <Text style={styles.modalBtn}>添加</Text>
+        </Pressable>
+      </Modal>
+    </Fragment>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     position: 'relative',
+    padding: 12,
+    paddingBottom: 100,
   },
   list: {
     minHeight: 600,
@@ -112,9 +157,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     position: 'absolute',
-    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
     height: 100,
-    width: '100%',
     borderTopWidth: 1,
     borderTopColor: 'rgba(0, 0, 0, .23)',
     paddingLeft: 20,
@@ -124,6 +170,39 @@ const styles = StyleSheet.create({
   },
   renderItem: {
     width: '100%',
+    marginBottom: 12,
+  },
+  modalContainer: {
+    padding: 20,
+    flex: 1,
+  },
+  title: {
+    marginBottom: 12,
+    color: Color.black,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: Color.grey,
+    borderStyle: 'solid',
+    borderRadius: 6,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  modalBtnWrapper: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    backgroundColor: Color.primary,
+    height: 50,
+    borderRadius: 6,
+    width: '80%',
+    marginLeft: 40,
+  },
+  modalBtn: {
+    color: FontColor.white,
+    lineHeight: 50,
+    textAlign: 'center',
+    fontSize: 20,
   },
 });
 
